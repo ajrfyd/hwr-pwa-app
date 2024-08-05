b60cf1257850721ce6c1fd0612ee4f640d978fd3
ajrfyd <61442171+ajrfyd@users.noreply.github.com>
2024-08-05 23:33:30 +0900

    Update

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,34 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico'],
+      manifest: {
+        name: 'Hanwoori Bus',
+        short_name: 'HanBus',
+        theme_color: '#fff',
+        icons: [
+          {
+            src: 'icons/favicon-32x32.png',
+            type: 'image/png',
+            sizes: '32x32'
+          },
+          {
+            src: 'icons/favicon-96x96.png',
+            type: 'image/png',
+            sizes: '96x96'
+          }
+        ]
+      }
+    })
+  ],
   resolve: {
     alias: [
       {
@@ -18,5 +42,8 @@ export default defineConfig({
   },
   build: {
     outDir: './build'
+  },
+  preview: {
+    port: 5000
   }
 });
