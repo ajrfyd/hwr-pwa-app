7c552bc68593295d3ffdf97e3dfdb1549ce872d1
ajrfyd <61442171+ajrfyd@users.noreply.github.com>
2024-08-13 00:11:20 +0900

    Update

--- server/src/d.ts
+++ server/src/d.ts
@@ -1,7 +1,7 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      PORT: number;
+      PORT: string;
       WEB_PUSH_PUBLIC_KEY: string;
       WEB_PUSH_PRIVATE_KEY: string;
     }
