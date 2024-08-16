785d98adc66a2761cfe30172b239d12dc7616e5a
ajrfyd <61442171+ajrfyd@users.noreply.github.com>
2024-08-16 22:13:36 +0900

    Update

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,5 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(parseInt(process.env.PORT));
+  console.log(1);
 }
 bootstrap();
